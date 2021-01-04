5a5934867ffa42cf4f0dcac742fe933788b6429d
Aleksei Smantser <lesha_smancer@mail.ru>
2021-01-04 14:50:45 +0300

    read links and save to files

--- src/fxp/parser/parsers.py
+++ src/fxp/parser/parsers.py
@@ -9,38 +9,30 @@ from abc import ABC, abstractmethod
 from fxp.parser import HOST, PREVIEW_URL, BASE_DIR
 
 class BaseParser(ABC):
-    def __init__(self, user_agent:str = None):
-        pass
-        # self._user_agent = user_agent if user_agent is not None else DEFAULT_USER_AGENT
-
     def _get_page(self, url):
-        # response = requests.get(
-        #     url, 
-        #     headers={"User-Agent": self._user_agent}
-        # )
         response = requests.get(url)
         if response.status_code == 200:
             return BS(response.text, features="html.parser")
         raise ValueError("Response not 200")
 
-    # @abstractmethod
-    # def save_to_file(self, name: str) -> None:
-    #     """Save news to file
+    @abstractmethod
+    def save_to_file(self, name: str) -> None:
+        """Save news to file
 
-    #     Args:
-    #         name (str): file name
-    #     """
+        Args:
+            name (str): file name
+        """
 
-    # def save_to_json(self, path: str) -> None:
-    #     """Save news to json file
+    @abstractmethod
+    def save_to_json(self, path: str) -> None:
+        """Save news to json file
 
-    #     Args:
-    #         path (str): file_name
-    #     """
+        Args:
+            path (str): file_name
+        """
 
 class Preview(BaseParser):
     def __init__(self, **kwargs):
-        # super().__init__(kwargs.get("user_agent"))
         now = datetime.now()
         temp_date = now.strftime("%d.%m.%Y")
         self.__num_page = kwargs.get("page") or temp_date
@@ -49,14 +41,35 @@ class Preview(BaseParser):
     def get_links(self):
         try:
             html = self._get_page(PREVIEW_URL.format(HOST, self.__num_page))
-            print(html)
-        except ValueError as error:
-            print(error)
+        except ValueError:
             self.__links = []
-        # else:
-        #     box = html.find()
+        else:
+            top_box = html.find_all("div", attrs={"class": "news-top"})
+            box = html.find_all("div", attrs={"class": "b-news"})
+            for i in box:
+                top_box.append(i)
+            if top_box is not None:
+                for rubric in top_box:
+                    box2 = rubric.find_all("div", attrs={"class": "news-entry"})
+                    if box2 is not None:
+                        for a in box2:
+                            link = a.find("a", attrs={"class": "entry__link"})
+                            print(link.get("href"), end='\n\n')
+                            self.__links.append(link.get("href"))
+            else:
+                self.__list = []
+
+    def save_to_file(self, name):
+        path = os.path.join(BASE_DIR, name + ".bin")
+        pickle.dump(self.__links, open(path, "wb"))
+
+    def save_to_json(self, name):
+        path = os.path.join(BASE_DIR, name + ".json")
+        json.dump(self.__links, open(path, "w"))
 
 
 if __name__ == "__main__":
-    parser = Preview(page="03.10.2000")
+    parser = Preview(page="03.01.2021")
     parser.get_links()
+    parser.save_to_json("03.01.2021")
+    parser.save_to_file("03.01.2021")
