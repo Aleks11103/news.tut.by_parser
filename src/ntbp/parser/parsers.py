1d08d27da70b5da80a242172b3d754588170d282
Aleksei Smantser <lesha_smancer@mail.ru>
2021-01-26 20:17:09 +0300

    Add save news in DB

--- src/ntbp/parser/parsers.py
+++ src/ntbp/parser/parsers.py
@@ -3,8 +3,10 @@ import json
 import pickle
 import requests
 import time
+import sqlite3
+from random import randint
 from threading import Lock
-from datetime import datetime
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup as BS
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
@@ -69,6 +71,8 @@ class BaseParser(BaseMeta):
 
 class Preview(BaseParser):
     def __init__(self, **kwargs):
+        global count  #
+        count = 1   #
         now = datetime.now()
         temp_date = now.strftime("%d.%m.%Y")
         self.__num_page = kwargs.get("page") if kwargs.get("page") is not None else temp_date
@@ -140,12 +144,12 @@ class NewsParser(BaseParser):
         self.news = {}
 
     def __call__(self, url):
-        with lock:
-            try:
-                html = self._get_page(url)
-            except ValueError as error:
-                print(error)
-            else:
+        try:
+            html = self._get_page(url)
+        except ValueError as error:
+            print(error)
+        else:
+            with lock:
                 box = html.find("div", attrs={"class": "b-article"})
                 if box is not None:
                     self.news["head"] = box.find("h1").text
@@ -153,14 +157,33 @@ class NewsParser(BaseParser):
                     if box_date is not None:
                         self.news["date"] = datetime.fromisoformat(box_date.get("datetime")).timestamp()
                     list_img =  box.find_all("img")
-                    self.news["src_img"] = []
+                    self.news["img_link"] = []
                     if list_img is not None:
                         for img in list_img:
-                            self.news["src_img"].append(img.attrs["src"])
+                            self.news["img_link"].append(img.attrs["src"])
                     if box.find("div", attrs={"id": "article_body"}) is not None:
                         text_block = box.find("div", attrs={"id": "article_body"}).text
                         self.news["text"] = text_block 
-                    self.save_to_json()
+                    list_source_link = box.find_all("a", attrs={"itemprop": "sameAs"})
+                    if list_source_link is not None:
+                        link_list = []
+                        for el in list_source_link:
+                            link_list.append(el.attrs["href"])
+                        self.news["source_link"] = link_list
+                    else:
+                        self.news["source_link"] = ["news.tut.by"]
+                    a_rubric = html.find("div", attrs={"class": "b-label"})
+                    self.news["rubric"] = a_rubric.find("a").text
+                    self.news["tags"] = []
+                    box_tag = html.find("ul", attrs={"class": "b-article-info-tags"})
+                    if box_tag is not None:
+                        list_tag = box_tag.find_all("a")
+                        for a in list_tag:
+                            self.news["tags"].append(a.text)
+                    # print(count, self.news["tags"])
+                    # self.save_to_json()
+                    self.save_to_db()
+                    print(self.news["head"] + "\tOK...")
 
     def save_to_json(self):
         global count
@@ -183,41 +206,145 @@ class NewsParser(BaseParser):
         json.dump(self.news, open(path, "w", encoding="utf-8"), ensure_ascii=False)
         count += 1
 
-
-    # def save_to_json(self, name):
-    #     in_path = os.path.join(*name)   #
-    #     path = os.path.join(BASE_DIR, in_path + ".json")    #
-    #     dirs = os.path.split(path)[:-1]
-    #     try:
-    #         os.makedirs(os.path.join(*dirs))
-    #     except Exception as error:
-    #         print(error)
-    #     json.dump(self.news, open(path, "w", encoding="utf-8"), ensure_ascii=False)
+    def save_to_db(self):
+        connection = sqlite3.connect("news_tut_by.db")
+        cursor = connection.cursor()
+        sql = """
+            --1
+            CREATE TABLE IF NOT EXISTS rubrics(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                "name" TEXT(30) NOT NULL UNIQUE
+            );
+            
+            --2
+            CREATE TABLE IF NOT EXISTS "sources"(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                "name" TEXT(70) NOT NULL UNIQUE
+            );
+            
+            --3
+            CREATE TABLE IF NOT EXISTS tags(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                "name" TEXT(30) NOT NULL UNIQUE
+            );
+            
+            --4
+            CREATE TABLE IF NOT EXISTS news(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                title TEXT(256) NOT NULL,
+                "text" TEXT NOT NULL,
+                "timestamp" REAL NOT NULL,
+                img_link TEXT,
+                rubric_id INTEGER NOT NULL,
+                UNIQUE (title, timestamp),
+                FOREIGN KEY (rubric_id) REFERENCES rubrics(id) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+            
+            --5
+            CREATE TABLE IF NOT EXISTS tags_news(
+                id_tag INTEGER NOT NULL,
+                id_news INTEGER NOT NULL,
+                FOREIGN KEY (id_tag) REFERENCES tags(id),
+                FOREIGN KEY (id_news) REFERENCES news(id)
+            );
+            
+            --6
+            CREATE TABLE IF NOT EXISTS sources_news(
+                id_source INTEGER NOT NULL,
+                id_news INTEGER NOT NULL,
+                FOREIGN KEY (id_source) REFERENCES sources(id),
+                FOREIGN KEY (id_news) REFERENCES news(id)
+            );"""
+        cursor.executescript(sql)
+        # list_rubrics = [("Sport",), ("Politic",), ("Finance",), ("IT",), ("Sport",), ("IT",), ("Sport",)]
+        cursor.execute("INSERT OR IGNORE INTO rubrics (name) VALUES(?)", (self.news["rubric"],))
+        list_sources = []
+        for el in self.news["source_link"]:
+            list_sources.append((el,))
+        list_tags = []
+        if len(self.news["tags"]) >= 1:
+            for el in self.news["tags"]:
+                list_tags.append((el,))
+        
+        cursor.executemany("INSERT OR IGNORE INTO sources (name) VALUES(?)", list_sources)
+        cursor.executemany("INSERT OR IGNORE INTO tags (name) VALUES(?)", list_tags)
+        list_news = [
+            ("TUT.BY", "blablablabla", 82934.4, "asdkkm.jpg", 3), 
+            ("news2", "blablablabla2", 345435.2, "hasdoahd.jpg", 1)
+        ]
+        if len(self.news["img_link"]) >= 1:
+            str_link = ", ".join(self.news["img_link"])
+            news = (self.news["head"], self.news["text"], self.news["date"], str_link, self.news["rubric"])
+        else:
+            news = (self.news["head"], self.news["text"], self.news["date"], None, self.news["rubric"])
+        cursor.execute("INSERT OR IGNORE INTO news (title, text, timestamp, img_link, rubric_id) VALUES(?, ?, ?, ?, (SELECT id FROM rubrics WHERE name = ?))", news)
+        list_tags_news = []
+        for el in self.news["tags"]:
+            list_tags_news.append((el, self.news["head"], self.news["date"]))
+        cursor.executemany("INSERT OR IGNORE INTO tags_news (id_tag, id_news) VALUES ((SELECT id FROM tags WHERE name = ?), (SELECT id FROM news WHERE title = ? and timestamp = ?))", list_tags_news)
+        list_sources_news = []
+        for el in self.news["source_link"]:
+            list_sources_news.append((el, self.news["head"], self.news["date"]))
+        cursor.executemany("INSERT OR IGNORE INTO sources_news (id_source, id_news) VALUES((SELECT id FROM sources WHERE name = ?), (SELECT id FROM news WHERE title = ? and timestamp = ?))", list_sources_news)
+        connection.commit()
+        connection.close()
 
 
 if __name__ == "__main__":
+    while True:
+        str_date = ""
+        path = os.path.join(BASE_DIR, 'save_date.txt')
+        with open(path, 'r', encoding='utf-8') as f:
+            str_date = f.readline()
+        date = datetime.strptime(str_date, "%d.%m.%Y")
+        now = datetime.now()
+        str_temp_date = datetime.strftime(now, "%d.%m.%Y")
+        temp_date = datetime.strptime(str_temp_date, "%d.%m.%Y")
+        if date >= temp_date:
+            print("Ошибка даты, попробуйте завтра!", "Дата для анализа: " + str_date, "Текущая дата: " + str_temp_date, sep="\n")
+            break
+        parser = Preview(page=str_date)
+        parser.get_links()
+        news = NewsParser()
+        for pars in parser:
+            news.__call__(pars)
+        day_delta = timedelta(days=1)
+        print(date, day_delta)
+        date += day_delta
+        str_date = datetime.strftime(date, "%d.%m.%Y")
+        sleep_min = randint(1,30)
+        print("\nСледующая дата(", str_date, ")проанализируется через", sleep_min, "минут\n")
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(str_date)
+        time.sleep(60 * sleep_min)
+    
     # parser = Preview(page="03.10.2000")
-    parser = Preview(page="19.01.2021")
-    parser.get_links()
+    # parser = Preview(page="19.01.2021")
+    # parser.get_links()
+    
     # print(parser[1])
     # print(parser[1:4])
     # print(parser[-5:-1:2])
     # print(parser["key"])
     # print(parser[3.4])
     
-    news = NewsParser()
+    # news = NewsParser()
+    # for pars in parser:
+    #     news.__call__(pars)
+    
+    # news = NewsParser()
     # news.__call__(parser[0])
-    pool = ThreadPoolExecutor()
-    print(parser._Preview__links.__len__())
-    start = datetime.now()
-    time.sleep(2)
-    news_from_page = pool.map(news, parser)
-    for n in news_from_page:
-        pass
+    # pool = ThreadPoolExecutor(max_workers=1)
+    # print(parser._Preview__links.__len__())
+    # start = datetime.now()
+    # time.sleep(2)
+    # news_from_page = pool.map(news, parser)
+    # for n in news_from_page:
+        # pass
         # print(n)
         # print("=" * 150)
-    print(datetime.now() - start)
-    print(parser._Preview__links.__len__())
+    # print(datetime.now() - start)
+    # print(parser._Preview__links.__len__())
 
     # parser.save_to_json("03.10.2000")
     # parser.save_to_file("03.10.2000")
